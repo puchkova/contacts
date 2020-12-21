a62866e2ae268f846e61ef26918378a473dcf09e
tatjana.putskova <tatjana.putskova@ttu.ee>
2020-12-22 00:39:07 +0200

    Initial commit

---
+++ src/main/java/ee/kuehnenagel/contacts/contacts/ContactsApplication.java
@@ -0,0 +1,13 @@
+package ee.kuehnenagel.contacts.contacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/kuehnenagel/contacts/contacts/ContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.kuehnenagel.contacts.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
