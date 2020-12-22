f610da6d36c5186af47afd5d85f9329ce1a1e889
tatjana.putskova <tatjana.putskova@ttu.ee>
2020-12-23 00:02:24 +0200

    change autowired annotation to constructor

--- src/main/java/ee/kuehnenagel/contacts/service/ContactService.java
+++ src/main/java/ee/kuehnenagel/contacts/service/ContactService.java
@@ -3,16 +3,16 @@ package ee.kuehnenagel.contacts.service;
 import ee.kuehnenagel.contacts.model.Contact;
 import ee.kuehnenagel.contacts.repository.ContactRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+@RequiredArgsConstructor
 @Service
 public class ContactService {
 
-    @Autowired
-    private ContactRepository repository;
+    private final ContactRepository repository;
 
     public ArrayList<Contact> findAll(String name) {
         return repository.findAll(name);
