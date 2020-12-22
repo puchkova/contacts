1d5291429d9fa1dc21443912d073355d1c28a1ee
tatjana.putskova <tatjana.putskova@ttu.ee>
2020-12-23 01:46:13 +0200

    fix imports

--- src/main/java/ee/kuehnenagel/contacts/controller/ContactController.java
+++ src/main/java/ee/kuehnenagel/contacts/controller/ContactController.java
@@ -3,7 +3,11 @@ package ee.kuehnenagel.contacts.controller;
 import ee.kuehnenagel.contacts.model.Contact;
 import ee.kuehnenagel.contacts.service.ContactService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
