7efe534b4d32076ae5cf08ac54913c880ff4e1f6
tatjana.putskova <tatjana.putskova@ttu.ee>
2020-12-23 02:26:38 +0200

    repository method refactoring

--- src/main/java/ee/kuehnenagel/contacts/repository/ContactRepositoryImpl.java
+++ src/main/java/ee/kuehnenagel/contacts/repository/ContactRepositoryImpl.java
@@ -1,67 +1,48 @@
 package ee.kuehnenagel.contacts.repository;
 
 import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
 import ee.kuehnenagel.contacts.model.Contact;
-import ee.kuehnenagel.contacts.service.ContactService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
+@RequiredArgsConstructor
 @Repository
 public class ContactRepositoryImpl implements ContactRepository {
 
-    private final ArrayList contacts;
-
-    public ContactRepositoryImpl() {
-        contacts = new ArrayList();
-    }
+    private final ArrayList<Contact> contacts;
 
     @Override
     public ArrayList<Contact> findAll(String name) {
-        FileInputStream fis = null;
+       try {
+           String fileName = "src/main/resources/people.csv";
+           FileInputStream fis = new FileInputStream(new File(fileName));
+           CSVReader reader = new CSVReader(new InputStreamReader(fis));
+           String[] nextLine;
+           reader.readNext();
 
-        try {
-            String fileName = "src/main/resources/people.csv";
+           contacts.clear();
+           while ((nextLine = reader.readNext()) != null) {
 
-            fis = new FileInputStream(new File(fileName));
-            CSVReader reader = new CSVReader(new InputStreamReader(fis));
-            String[] nextLine;
-            reader.readNext();
+               Contact newContact = new Contact(nextLine[0],
+                       nextLine[1]);
 
-            contacts.clear();
-            while ((nextLine = reader.readNext()) != null) {
-
-                Contact newContact = new Contact(nextLine[0],
-                        nextLine[1]);
-
-                if (name == null) {
-                    contacts.add(newContact);
-                } else {
-                    if (newContact.getName().toLowerCase().contains(name.toLowerCase())) {
-                        contacts.add(newContact);
-                    }
-                }
-            }
-
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(ContactService.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(ContactService.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (CsvValidationException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (fis != null) {
-                    fis.close();
-                }
-            } catch (IOException ex) {
-                Logger.getLogger(ContactService.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+               if (name == null) {
+                   contacts.add(newContact);
+               } else {
+                   if (newContact.getName().toLowerCase().contains(name.toLowerCase())) {
+                       contacts.add(newContact);
+                   }
+               }
+           }
+           fis.close();
+    } catch (Exception e) {
+        throw new RuntimeException(e);
+    }
         return contacts;
     }
 }
