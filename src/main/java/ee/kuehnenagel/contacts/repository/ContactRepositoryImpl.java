bfe1f84f0264b95c673bb84c8b0ec8f7462e3f37
tatjana.putskova <tatjana.putskova@ttu.ee>
2020-12-24 00:32:31 +0200

    add logic to avoid invalid input issue with extra commas in the csv file

--- src/main/java/ee/kuehnenagel/contacts/repository/ContactRepositoryImpl.java
+++ src/main/java/ee/kuehnenagel/contacts/repository/ContactRepositoryImpl.java
@@ -1,12 +1,13 @@
 package ee.kuehnenagel.contacts.repository;
 
-import ee.kuehnenagel.contacts.model.Contact;
-
 import com.opencsv.CSVReader;
+import ee.kuehnenagel.contacts.model.Contact;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 @RequiredArgsConstructor
@@ -15,27 +16,43 @@ public class ContactRepositoryImpl implements ContactRepository {
 
     @Override
     public ArrayList<Contact> getContactsFromCsvFile(String name, String file) {
-        ArrayList<Contact> contacts = new ArrayList<>();
+        ArrayList<Contact> contactList = new ArrayList<>();
+
         try {
             FileInputStream fis = new FileInputStream(new File(file));
             CSVReader reader = new CSVReader(new InputStreamReader(fis));
-            String[] nextLine;
+            String[] row;
             reader.readNext();
-            while ((nextLine = reader.readNext()) != null) {
-                Contact newContact = new Contact(nextLine[0],
-                        nextLine[1]);
 
-                filterContacts(name, newContact, contacts);
+            while ((row = reader.readNext()) != null) {
+
+                Contact newContact = createNewContact(row);
+                filterContacts(name, newContact, contactList);
             }
+
             fis.close();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        return contacts;
+
+        return contactList;
+    }
+
+    private Contact createNewContact(String[] row) {
+        String name;
+        String url;
+        if (row[2].isEmpty()) {
+            name = row[0];
+            url = row[1];
+        } else {
+            name = row[0] + row[1];
+            url = row[2];
+        }
+        return new Contact(name, url);
     }
 
     private void filterContacts(String name, Contact newContact, ArrayList<Contact> contacts) {
-        if (name != null && !name.trim().isEmpty() && name.length()!=0) {
+        if (name != null && !name.trim().isEmpty() && name.length() != 0) {
             if (newContact.getName().toLowerCase().contains(name.toLowerCase())) {
                 contacts.add(newContact);
             }
