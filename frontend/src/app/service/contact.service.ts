12ef809b35f12252a6ea71ed72160bee508d4d50
tatjana.putskova <tatjana.putskova@ttu.ee>
2020-12-23 23:24:34 +0200

    add conditional to contact.service.ts method

--- frontend/src/app/service/contact.service.ts
+++ frontend/src/app/service/contact.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from "rxjs";
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,9 +9,16 @@ export class ContactService {
 
   private baseUrl = 'http://localhost:8080/contacts';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-  public getContacts(name: string): Observable<any>{
-    return this.http.get(`${this.baseUrl}`+ "?name=" + name)
+  public getContacts(name: string): Observable<any> {
+    let url;
+    if (name !== null && name !== '' && name !== undefined) {
+      url = this.http.get(`${this.baseUrl}` + "?name=" + name);
+    } else {
+      url = this.http.get(`${this.baseUrl}`);
+    }
+    return url;
   }
 }
