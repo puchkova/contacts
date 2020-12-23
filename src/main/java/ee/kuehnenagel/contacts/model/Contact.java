eeac417d6860f27b9dbbbca494e1ef29c9157482
tatjana.putskova <tatjana.putskova@ttu.ee>
2020-12-24 01:47:40 +0200

    remove unnecessary @Setter from model class

--- src/main/java/ee/kuehnenagel/contacts/model/Contact.java
+++ src/main/java/ee/kuehnenagel/contacts/model/Contact.java
@@ -2,10 +2,8 @@ package ee.kuehnenagel.contacts.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class Contact {
     private String name;
